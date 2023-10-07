8b52e740d86807fd1e02392a95174644ba99904b
evan <evandromariano49@gmail.com>
2023-10-06 21:13:49 -0300

    Projeto multimódulo

---
+++ project-parent/src/test/java/com/eva/joseph/AppTest.java
@@ -0,0 +1,20 @@
+package com.eva.joseph;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
